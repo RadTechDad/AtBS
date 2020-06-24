ad2892b2e6106b324e5d1cd258cba0656fd21a6d
Darrell Dudics <git@darrelldudics.com>
2020-06-23 22:19:10 -0700

    Added some examples from ch 6

---
+++ Chapter 6/catnapping.py
@@ -0,0 +1,10 @@
+# catnapping.py
+# AtBS - Chapter 6
+# Code by Darrell Dudics
+
+print('''Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+
+Sincerely,
+Bob''')
